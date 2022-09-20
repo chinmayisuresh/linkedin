dcf7e37b7ae5c2d21643a3efd7fb9d37d8f5487d
chinmayisuresh <chinmayisuresh1998@email.com>
2022-09-20 11:17:59 +0530

    Header option

--- src/Header.js
+++ src/Header.js
@@ -2,18 +2,31 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications'
 
 function Header() {
   return (
   <div className='header'>
   <div className='header__left'>
-        <LinkedInIcon color='primary' sx={{ fontSize: 40 }}/>
+        <LinkedInIcon color='primary' fontSize='large'/>
         <div className='header__search'>
             <SearchIcon/>
             <input type='text'/>
         </div>
     </div>
-    <div className='header__right'></div>
+    <div className='header__right'>
+      <HeaderOption title='Home' Icon={HomeIcon}/>
+      <HeaderOption title='My network' Icon={SupervisorAccountIcon}/>
+      <HeaderOption Icon={BusinessCenterIcon} title='jobs'/>
+      <HeaderOption Icon={ChatIcon} title='Messaging'/>
+      <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+      <HeaderOption avatar='https://via.placeholder.com/150' title='me'/>
+    </div>
   </div>
   )
 }
